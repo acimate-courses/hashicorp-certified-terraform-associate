61dd12bacbe5689a3eefa80c1f2c56336c738be2
Sripada Mishra <sripada.mishra@gmail.com>
2024-03-19 12:36:06 +0530

    added dynamic block

--- 11-terraform-dynamic-block/11-01-terraform-dynamic-block/secgroup.tf
+++ 11-terraform-dynamic-block/11-01-terraform-dynamic-block/secgroup.tf
@@ -1,30 +1,31 @@
+locals {
+    inbound_ports = [80, 443]
+    outbound_ports = [443, 1433]
+}
+
 # Security Groups
 resource "aws_security_group" "sg-webserver" {
     vpc_id              = aws_vpc.vpc.id
     name                = "webserver"
     description         = "Security Group for Web Servers"
-    ingress {
-        protocol = "tcp"
-        from_port = 80
-        to_port = 80
-        cidr_blocks = [ "0.0.0.0/0" ]
+
+    dynamic "ingress" {
+        for_each = local.inbound_ports
+        content {
+            from_port   = ingress.value
+            to_port     = ingress.value
+            protocol    = "tcp"
+            cidr_blocks = [ "0.0.0.0/0" ]
+        }
     }
-    ingress {
-        protocol = "tcp"
-        from_port = 443
-        to_port = 443
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-    egress {
-        protocol = "tcp"
-        from_port = 443
-        to_port = 443
-        cidr_blocks = [ var.vpc-cidr ]
-    }
-    egress {
-        protocol = "tcp"
-        from_port = 1433
-        to_port = 1433
-        cidr_blocks = [ var.vpc-cidr ]
+
+    dynamic "egress" {
+        for_each = local.outbound_ports
+        content {
+            from_port   = egress.value
+            to_port     = egress.value
+            protocol    = "tcp"
+            cidr_blocks = [ var.vpc-cidr ]
+        }
     }
 }
